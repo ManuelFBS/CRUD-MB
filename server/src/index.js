977b45710478694121e45d88ce3dc06929b11401
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-23 16:17:38 -0400

    Actualizacion de index.js (server)

--- server/src/index.js
+++ server/src/index.js
@@ -1,9 +1,9 @@
 import express from 'express';
-// import mysql from 'mysql';
-// import myconn from 'express-myconnection';
+import mysql from 'mysql2';
+import myconn from 'express-myconnection';
 import cors from 'cors';
 import morgan from 'morgan';
-// import { dbOptions } from './config/db.mjs';
+import { dbOptions } from './config/db.js';
 // import indexRouter from './routes/index.route.mjs';
 
 const app = express();
@@ -11,11 +11,12 @@ const app = express();
 app.set('port', process.env.PORT || 8000);
 
 // Middlewares... --------------------------------------------------------------------------------------------------------
-// app.use(myconn(mysql, dbOptions, 'single'));
+app.use(myconn(mysql, dbOptions, 'single'));
 app.use(express.json());
 app.use(morgan('dev'));
 app.use(cors());
 
 app.listen(app.get('port'), () => {
   console.log('Server is running on port: ', app.get('port'));
+  console.log('Connecting with database...');
 });
