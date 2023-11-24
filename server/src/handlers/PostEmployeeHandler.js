539c0a6239236f9177b666da1935cb9f9f01112f
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-24 02:17:31 -0400

    Creacion de PostEmployeeHandler.js

---
+++ server/src/handlers/PostEmployeeHandler.js
@@ -0,0 +1,20 @@
+import { createEmployee } from '../controllers/PostEmployeeController.js';
+
+export const createEmployeeHandler = async (req, res) => {
+  try {
+    const employee = req.body;
+    const result = await createEmployee(emoployee);
+
+    res.status(201).json({
+      message: 'The employee has been successfully registered...!!!',
+      data: employee
+    });
+
+    return result;
+  } catch (error) {
+    res.status(500).json({
+      message: 'Internal Server Error...',
+      error: error.message
+    });
+  }
+};
