f3e1262f0cb39bd013f9173cf1539feca458e136
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-24 09:55:57 -0400

    Modific. en controlador, handler de PostEmployee.

--- server/src/controllers/PostEmployeeController.js
+++ server/src/controllers/PostEmployeeController.js
@@ -2,8 +2,16 @@ import { executeQuery } from '../config/database.js';
 
 export const createEmployee = async (employee) => {
   try {
-    const query = `INSERT INTO employee (name, age, country, position, years) VALUES (${employee.name}, ${employee.age}, ${employee.country}, ${employee.position}, ${employee.years})`;
-    const result = await executeQuery(query);
+    const query =
+      'INSERT INTO employee (name, age, country, position, years) VALUES (?, ?, ?, ?, ?)';
+    const values = [
+      employee.name,
+      employee.age,
+      employee.country,
+      employee.position,
+      employee.years
+    ];
+    const result = await executeQuery(query, values);
 
     return result;
   } catch (error) {
--- server/src/handlers/PostEmployeeHandler.js
+++ server/src/handlers/PostEmployeeHandler.js
@@ -3,7 +3,7 @@ import { createEmployee } from '../controllers/PostEmployeeController.js';
 export const createEmployeeHandler = async (req, res) => {
   try {
     const employee = req.body;
-    const result = await createEmployee(emoployee);
+    const result = await createEmployee(employee);
 
     res.status(201).json({
       message: 'The employee has been successfully registered...!!!',
