ce17b72f6f637b3cc676fe0c75c84797c591a43e
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-24 00:55:44 -0400

    Modificacion en index.route.js e index.js (server)

--- server/src/routes/index.route.js
+++ server/src/routes/index.route.js
@@ -1,4 +1,5 @@
 import { Router } from 'express';
+import * as getEmpHand from '../handlers/GetEmployeesHandler.js';
 
 export const routes = Router();
 
@@ -7,3 +8,5 @@ const prefix = '/api';
 routes.get(prefix, (rer, res) => {
   res.send('Welcome to the API Rest Employee...!!!');
 });
+
+routes.get(`${prefix}/employees`, getEmpHand.getAllEmployeesHandler);
