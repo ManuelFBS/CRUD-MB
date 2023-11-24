e97783fc18f8788840a9e57073b8eec381be2ea4
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-23 23:16:20 -0400

    Creacion de GetEmployeesController.js

---
+++ server/src/controllers/GetEmployeesController.js
@@ -0,0 +1,8 @@
+import { dbOptions } from '../config/db.js';
+
+export const getAllEmployee = async () => {
+  try {
+    const query = 'SELECT * FROM employee';
+    // onst result = await dbOptions.
+  } catch (error) {}
+};
