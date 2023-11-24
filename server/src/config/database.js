abb5cf1a6f4d4b77adf724594d5894598075be86
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-24 10:09:37 -0400

    Modificacion de database.js

--- server/src/config/database.js
+++ server/src/config/database.js
@@ -1,13 +1,13 @@
 import mysql from 'mysql2';
 import { dbOptions } from './dbConfig.js';
 
-export const executeQuery = (query) => {
+export const executeQuery = (query, values) => {
   return new Promise((resolve, reject) => {
     const connection = mysql.createConnection(dbOptions);
 
     connection.connect();
 
-    connection.query(query, (err, results) => {
+    connection.query(query, values, (err, results) => {
       connection.end;
 
       if (err) {
