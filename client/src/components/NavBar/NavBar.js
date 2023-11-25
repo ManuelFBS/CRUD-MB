4cb943f845678b953d6a2d2bf52bf754ffa06447
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-25 02:48:38 -0400

    Creacion de NavBar.js

---
+++ client/src/components/NavBar/NavBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './NavBar.css';
+
+export const NavBar = () => {
+  return (
+    <div className='bigContainer'>
+      <nav className='container'>
+        <div className='navbarDiv'>Employees App</div>
+      </nav>
+    </div>
+  );
+};
