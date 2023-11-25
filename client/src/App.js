b8110b609ec9523639d1315fce7145a0ae122a1c
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-25 03:35:16 -0400

    Actualizacion de App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,16 @@
 // import React, { Fragment, useEffect, useState } from 'react';
 import React, { Fragment } from 'react';
 import { NavBar } from './components/NavBar/NavBar.js';
+import { Form } from './components/Form/Form.js';
 // import Axios from 'axios';
 
 function App() {
   return (
     <Fragment>
       <NavBar />
+      <div>
+        <Form />
+      </div>
     </Fragment>
   );
 }
