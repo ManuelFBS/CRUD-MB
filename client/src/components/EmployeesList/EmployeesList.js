40be8ccc869c7d4ea5f7388faeea0ed0599ffe9b
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-28 12:27:15 -0400

    Actualización y modificación de EmployeesList.js

--- client/src/components/EmployeesList/EmployeesList.js
+++ client/src/components/EmployeesList/EmployeesList.js
@@ -1,5 +1,7 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './EmployeesList.css';
+import Axios from 'axios';
+import swal from 'sweetalert2';
 
 export const EmployeesList = ({
   employee,
@@ -7,19 +9,67 @@ export const EmployeesList = ({
   employees,
   setListEmployeesUpdated
 }) => {
-  console.log('Employees', employees);
+  // console.log('Employees', employees);
+  const [loading, setLoading] = useState(false);
 
   if (!employees || employees.length === 0) {
     return <p>No hay empleados disponibles.</p>;
   }
-  // Reiniciando state del employee...
-  // setEmployee({
-  //   name: '',
-  //   age: 0,
-  //   country: '',
-  //   position: '',
-  //   years: 0
-  // });
+
+  const handleUpdate = async (id) => {
+    try {
+      const updatedFields = Object.fromEntries(
+        Object.entries(employee).filter(([key, value]) => value !== '')
+      );
+
+      const requestInit = {
+        method: 'PATCH',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify(updatedFields)
+      };
+
+      setLoading(true);
+
+      await Axios.patch(
+        'http://localhost:8000/api/employees/update/' + id,
+        requestInit
+      );
+
+      // Se limpia el Form después de hacer la petición...
+      setEmployee({
+        name: '',
+        age: '',
+        country: '',
+        position: '',
+        years: ''
+      });
+
+      // Otras acciones después de actualizar, como mostrar una alerta
+      // swal.fire('Éxito', 'Registro actualizado correctamente', 'success');
+      swal.fire({
+        title: 'Exito...',
+        text: 'Registro actualizado correctamente...!!!',
+        icon: 'success',
+        showConfirmButton: true,
+        confirmButtonText: 'Aceptar'
+      });
+    } catch (error) {
+      console.error('Error al guardar el registro:', error);
+      // Mostrar una alerta de error si es necesario
+      // swal.fire('Error', 'No se pudo actualizar el registro', 'error');
+      swal.fire({
+        title: 'error',
+        text: error.message,
+        icon: 'error',
+        showConfirmButton: true,
+        confirmButtonText: 'Aceptar'
+      });
+    } finally {
+      setLoading(false);
+    }
+
+    setListEmployeesUpdated(true);
+  };
 
   return (
     <table>
@@ -46,7 +96,12 @@ export const EmployeesList = ({
             <td>{employeeItem.years}</td>
             <td className='tdButton'>
               <div>
-                <button className='butSave'>Editar</button>
+                <button
+                  className='butSave'
+                  onClick={() => handleUpdate(employee.id)}
+                >
+                  {loading ? 'Editando' : 'Editar'}
+                </button>
               </div>
             </td>
             <td className='tdButton'>
