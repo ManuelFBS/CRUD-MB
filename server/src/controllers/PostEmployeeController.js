f214e6ae0af41cadabe24f30bb7987d26f73df0a
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-28 06:50:58 -0400

    Modificacion de PostEmployeeController.js

--- server/src/controllers/PostEmployeeController.js
+++ server/src/controllers/PostEmployeeController.js
@@ -17,21 +17,8 @@ export const createEmployee = async (employee) => {
       employee.years
     ];
     const result = await executeQuery(query, values);
-
-    // return result;
-    return {
-      success: true,
-      message: 'El empleaddo ha sido registrado correctamente...!!!',
-      data: employee
-    };
+    return result;
   } catch (error) {
-    // throw error;
-    console.error('Error al tratar de crear el empleado...!');
-
-    return {
-      success: false,
-      message: 'Ha ocurrido un error al tratar de crear el empleado...',
-      error: error.message
-    };
+    throw error;
   }
 };
