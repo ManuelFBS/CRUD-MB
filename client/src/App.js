f0784d8bfc03c3d70aa164f88471824f9071d1ba
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-30 10:58:01 -0400

    Modificacion en los estilos

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React, { Fragment, useEffect, useState } from 'react';
 import Axios from 'axios';
 import { NavBar } from './components/NavBar/NavBar.js';
 import { Form } from './components/Form/FormInit.js';
-import './App.css';
+// import './App.css';
 import { EmployeesList } from './components/EmployeesList/EmployeesListInit.js';
 
 function App() {
@@ -34,27 +34,52 @@ function App() {
   }, [listEmployeesUpdated]);
 
   return (
-    <Fragment>
-      <NavBar />
-      <div>
-        <Form
-          employee={employee}
-          setEmployee={setEmployee}
-          setListEmployeesUpdated={setListEmployeesUpdated}
-        />
-      </div>
-      <div id='main-container'>
-        <h2 className='titList'>Lista de Empleados</h2>
-        <div>
-          <EmployeesList
+    // <Fragment>
+    //   <NavBar />
+    //   <div>
+    //     <Form
+    //       employee={employee}
+    //       setEmployee={setEmployee}
+    //       setListEmployeesUpdated={setListEmployeesUpdated}
+    //     />
+    //   </div>
+    //   <div id='main-container'>
+    //     <h2 className='titList'>Lista de Empleados</h2>
+    //     <div>
+    //       <EmployeesList
+    //         employee={employee}
+    //         setEmployee={setEmployee}
+    //         employees={employees}
+    //         setListEmployeesUpdated={setListEmployeesUpdated}
+    //       />
+    //     </div>
+    //   </div>
+    // </Fragment>
+    <div id='main-container'>
+      <Fragment>
+        <div className='nav-container'>
+          <NavBar />
+        </div>
+        <div id='form-container'>
+          <Form
             employee={employee}
             setEmployee={setEmployee}
-            employees={employees}
             setListEmployeesUpdated={setListEmployeesUpdated}
           />
         </div>
-      </div>
-    </Fragment>
+        <div id='list-container'>
+          <h2 className='titList'>Lista de Empleados</h2>
+          <div>
+            <EmployeesList
+              employee={employee}
+              setEmployee={setEmployee}
+              employees={employees}
+              setListEmployeesUpdated={setListEmployeesUpdated}
+            />
+          </div>
+        </div>
+      </Fragment>
+    </div>
   );
 }
 
