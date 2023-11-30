0297bf8c6924e82f66f00fd8086b691bcabdfdab
ManuelFBS <manuelf.borrego@gmail.com>
2023-11-30 10:38:17 -0400

    Modificacion de todos .css y de EmployeesListView.js

--- client/src/components/EmployeesList/EmployeesListView.js
+++ client/src/components/EmployeesList/EmployeesListView.js
@@ -8,51 +8,53 @@ export const EmployeesListView = ({
   loading
 }) => {
   return (
-    <table>
-      <thead>
-        <tr>
-          <th>ID</th>
-          <th>Nombre</th>
-          <th>Edad</th>
-          <th>País</th>
-          <th>Cargo</th>
-          <th>Años</th>
-          <th></th>
-          <th></th>
-        </tr>
-      </thead>
-      <tbody>
-        {employees.map((employeeItem) => (
-          <tr key={employeeItem.id}>
-            <td>{employeeItem.id}</td>
-            <td>{employeeItem.name}</td>
-            <td>{employeeItem.age}</td>
-            <td>{employeeItem.country}</td>
-            <td>{employeeItem.position}</td>
-            <td>{employeeItem.years}</td>
-            <td className='tdButton'>
-              <div>
-                <button
-                  className='butSave'
-                  onClick={() => handleUpdate(employeeItem.id)}
-                >
-                  {loading ? 'Editando' : 'Editar'}
-                </button>
-              </div>
-            </td>
-            <td className='tdButton'>
-              <div>
-                <button
-                  className='butDel'
-                  onClick={() => handleDelete(employeeItem.id)}
-                >
-                  {loading ? 'Eliminando' : 'Eliminar'}
-                </button>
-              </div>
-            </td>
+    <div className='table-Container'>
+      <table>
+        <thead>
+          <tr>
+            <th>ID</th>
+            <th>Nombre</th>
+            <th>Edad</th>
+            <th>País</th>
+            <th>Cargo</th>
+            <th>Años</th>
+            <th></th>
+            <th></th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody>
+          {employees.map((employeeItem) => (
+            <tr key={employeeItem.id}>
+              <td>{employeeItem.id}</td>
+              <td>{employeeItem.name}</td>
+              <td>{employeeItem.age}</td>
+              <td>{employeeItem.country}</td>
+              <td>{employeeItem.position}</td>
+              <td>{employeeItem.years}</td>
+              <td className='tdButton'>
+                <div>
+                  <button
+                    className='butSave'
+                    onClick={() => handleUpdate(employeeItem.id)}
+                  >
+                    {loading ? 'Editando' : 'Editar'}
+                  </button>
+                </div>
+              </td>
+              <td className='tdButton'>
+                <div>
+                  <button
+                    className='butDel'
+                    onClick={() => handleDelete(employeeItem.id)}
+                  >
+                    {loading ? 'Eliminando' : 'Eliminar'}
+                  </button>
+                </div>
+              </td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
   );
 };
